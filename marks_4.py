cce42b845f7ce356fe295f5891de120f8c861489
Architg021 <122766796+Architg021@users.noreply.github.com>
2023-02-12 17:58:07 +0530

    Create marks_4.py

---
+++ marks_4.py
@@ -0,0 +1,10 @@
+m1=int(input("Enter marks for Student1:"))
+m2=int(input("Enter marks for Student2:"))
+m3=int(input("Enter marks for Student3:"))
+m4=int(input("Enter marks for Student4:"))
+m5=int(input("Enter marks for Student5:"))
+m6=int(input("Enter marks for Student6:"))
+
+myList = [m1, m2, m3, m4, m5, m6]
+myList.sort()
+print(myList)
