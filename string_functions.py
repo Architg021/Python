35fac4d21e41da540f7812da912288b46d12aa43
Architg021 <122766796+Architg021@users.noreply.github.com>
2023-02-12 13:37:44 +0530

    Create string_functions.py

---
+++ string_functions.py
@@ -0,0 +1,9 @@
+story = "once upon a time there was student who was good"
+
+# String Functions
+# print(len(story))
+print(story.endswith("was"))
+# print(story.count("c"))
+# print(story.capitalize())
+#print(story.find("there")) in this it will also count spaces like time se pehle waali 17 hai toh vo hoga
+#print(story.replace("Harry", ""))
