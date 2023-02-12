6669d69df3e3958128012a423bc1c5d78967dacb
Architg021 <122766796+Architg021@users.noreply.github.com>
2023-02-12 21:03:43 +0530

    Create emoty_set.py

---
+++ emoty_set.py
@@ -0,0 +1,7 @@
+# Important: This syntax will create an empty dictionary and not an empty set
+a = {}
+print(type(a))
+
+# An empty set can be created using the below syntax:
+b = set()#if removed the word set here it will become tuple
+print(type(b))
