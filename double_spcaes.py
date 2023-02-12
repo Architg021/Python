3ce75abccbb689f4cdcd7c26a35f82067e7da4a3
Architg021 <122766796+Architg021@users.noreply.github.com>
2023-02-12 14:30:25 +0530

    Create double_spcaes.py

---
+++ double_spcaes.py
@@ -0,0 +1,5 @@
+string="Hi  how  are   you"'''The expression string.find(" ") finds the first occurrence of the substring " " 
+(double spaces) in the string st.thats why output will be always 2 if u dont remove deoubles paces after hi'''
+
+doublespaces=string.find("  ")
+print(doublespaces)
