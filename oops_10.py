4a0bfe4dce768227e82bdfd250b4266feff89418
Architg021 <122766796+Architg021@users.noreply.github.com>
2023-02-18 16:03:15 +0530

    Create oops_10.py

---
+++ oops_10.py
@@ -0,0 +1,23 @@
+class Number:
+    def sum(self):
+        return self.a + self.b
+
+num = Number()
+num.a = 12
+num.b = 34
+s = num.sum()
+print(s)
+
+# a = 12
+# b = 34
+
+# print("The sum of a and b is ", a+b)
+
+'''
+PascalCase 
+EmployeeName -->PascalCase #in this case first letter of alphabet would be big like in Employee E and in nam N
+
+camelCase #in this first will be small then second big then agard third small and fourth big like in isFloatorInt is
+i is amm then Float F is big then or o is small Int I is
+isNumeric, isFloatOrInt -->camelCase
+'''
